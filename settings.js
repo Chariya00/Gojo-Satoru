b9af7043286aef8e188fc249eecef3407f670568
Chariya00 <108907916+Chariya00@users.noreply.github.com>
2022-07-08 07:35:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94719868299']
+global.premium = ['94719868299']
+global.ownernomer = '94719868299'
+global.ownername = '😉Charith Damruwan😈'
+global.botname = 'Creat by Charith'
+global.footer = '©CharithDamruwan.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'mage eka'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
